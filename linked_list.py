c2746f67417e850d453c85e694b6a36f76482b39
Yin <yinnnn17@gmail.com>
2020-11-03 13:18:16 -0800

    Implement a prepend() method

--- linked_list.py
+++ linked_list.py
@@ -12,9 +12,17 @@ class  LinkedList(object):
   def append(self,item):
     """ A method to add an item to the end of the linked list """
     new_node = Node(item)
-    # If the linkedlist is empty
-    if self.head is None:
+    if !self.head: # Check if the linkedlist is empty
       self.head = new_node
     else:
       self.tail.next = new_node
     self.tail = new_node
+  def prepend(self,item):
+    """ A method to add an item to the beginning of the list """
+    new_node = Node(item)
+    if !self.head: # Check if the linkedlist is empty
+      self.head = new_node
+      self.tail = new_node
+    else:
+      new_node.next = self.head
+      self.head = new_node
