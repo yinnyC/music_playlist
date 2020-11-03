db1a903cd27a3a03d33a0d6d0b28cf87c2799325
Yin <yinnnn17@gmail.com>
2020-11-03 12:36:33 -0800

    Implement a append() method

--- linked_list.py
+++ linked_list.py
@@ -3,3 +3,18 @@ class Node(object):
     """ Initialize a new node with the given data """
     self.data = data
     self.next = None
+
+class  LinkedList(object):
+  def __init__(self):
+    """ Initialize this linked list. """
+    self.head = None
+    self.tail = None
+  def append(self,item):
+    """ A method to add an item to the end of the linked list """
+    new_node = Node(item)
+    # If the linkedlist is empty
+    if self.head is None:
+      self.head = new_node
+    else:
+      self.tail.next = new_node
+    self.tail = new_node
